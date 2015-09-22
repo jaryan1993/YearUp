3852af540336609359ca254da47913c091614662
jryan <jryan@yearup.org>
2015-09-22 11:28:41 -0400

    Project 1 Files

---
+++ src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro {
+
+	public static void main(String[] args) 
+	{
+		
+
+System.out.println("My name is Jahvariah");
+System.out.println("My favorite book is Holes by Louis Sahar");
+System.out.println("I enjoy taking long bike rides");
+}
+
+}
